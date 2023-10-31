9b4da4df664f6ab050ac07d96c05d6d2ad5885f3
dkblinux98 <darla.baker@bakewell.me>
2023-10-31 12:40:45 -0400

    added cloudfront cache invalidation to deployment

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,8 @@ npm run build
 # --delete removes files from S3 that are not present in the local folder
 aws s3 sync build/ s3://$BUCKET_NAME/ --acl public-read --delete
 
+# Invalidate CloudFront distribution cache
+DISTRIBUTION_ID=E2PK2XQ8TLXA80
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+
 echo "Deployment complete."
