cc0418a9281afde6f89e50f133b7f7bd9a0de846
Darla Baker <darlabaker@Darlas-MBP.lan>
2023-10-31 11:08:06 -0400

    added a deploy script to deploy the build to S3 for static serving

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set your bucket name here
+BUCKET_NAME=opusdigitalbadge
+
+# Build your project (React app)
+npm run build
+
+# Deploy to S3
+# --acl public-read makes the files publicly readable
+# --delete removes files from S3 that are not present in the local folder
+aws s3 sync build/ s3://$BUCKET_NAME/ --acl public-read --delete
+
+echo "Deployment complete."
